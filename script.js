ce8ff57950e83f51d6ae9e15be0d13d0c155d4d1
Hrn-Samy <samy.haroun78@gmail.com>
2026-02-08 19:51:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const year = document.getElementById("year");
 if (year) year.textContent = new Date().getFullYear();
 
+/* ===== Intro Animation FIX ===== */
+
+const intro = document.getElementById("intro");
+
+if (intro) {
+  // démarre l’animation
+  intro.classList.add("intro-show");
+
+  // cache après 2.2s
+  setTimeout(() => {
+    intro.classList.add("intro-hide");
+  }, 2200);
+}
+
+/* ===== Burger menu ===== */
+
 const burger = document.querySelector(".burger");
 const menu = document.querySelector("[data-menu]");
 
